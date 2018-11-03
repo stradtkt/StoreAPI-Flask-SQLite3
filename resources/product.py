2747fe1ff8432bb231532b0bb35bfc4498777507
Kevin Stradtman <stradtkt@gmail.com>
2018-11-03 17:20:03 -0400

    added the rest of product resource

--- resources/product.py
+++ resources/product.py
@@ -25,3 +25,42 @@ class Product(Resource):
         except:
             return {'message': 'An error has occurred adding the product'}, 500
         return product, 201
+
+    def delete(self, id):
+        connection = sqlite3.connect('data.db')
+        cursor = connection.cursor()
+        query = "DELETE FROM products WHERE id=?"
+        cursor.execute(query, (id,))
+        connection.commit()
+        connection.close()
+        return {'message': 'Product deleted'}
+
+    def put(name):
+        data = Product.parser.parse_args()
+        product = ProductModel.find_by_name(name)
+        updated_product = {'name': name, 'price': price, 'description': description}
+        if product is None:
+            try:
+                ProductModel.insert(updated_product)
+            except:
+                return {'message': 'An error occurred adding the product.'}
+        else:
+            try:
+                ProductModel.update(updated_product)
+            except:
+                return {'message': 'An error has occurred updating the product'}
+        return updated_product
+
+    
+class ProductList(Resource):
+    def get(self):
+        connection = sqlite3.connect('data.db')
+        cursor = connection.cursor()
+        query = "SELECT * FROM products"
+        result = cursor.execute(query)
+        products = []
+        for row in result:
+            products.append({'name': row[0], 'price': row[1], 'description': row[2]})
+
+        connection.close()
+        return {'products': products}
